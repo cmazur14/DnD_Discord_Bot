97907da614224c8f34c3b0f4b6de7ceb63984e9d
Chris Mazur <christopher.mazur@fedex.com>
2019-08-08 10:45:33 -0400

    Added empty test cases

---
+++ src/test/java/com/thegingerbeardd/dndbot/parsing/FifthEditionCommandParserTest.java
@@ -0,0 +1,5 @@
+package com.thegingerbeardd.dndbot.parsing;
+
+public class FifthEditionCommandParserTest {
+    //TODO write this
+}
---
+++ src/test/java/com/thegingerbeardd/dndbot/processor/impl/FifthEdtionChatProcessorTest.java
@@ -0,0 +1,5 @@
+package com.thegingerbeardd.dndbot.processor.impl;
+
+public class FifthEdtionChatProcessorTest {
+    //TODO implement this whole thing
+}
