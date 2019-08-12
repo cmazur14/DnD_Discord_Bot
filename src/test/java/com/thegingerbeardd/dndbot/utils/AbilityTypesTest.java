0f2379fcf60599ef7f20c724bd558f92944bf93b
Chris Mazur <christopher.mazur@fedex.com>
2019-08-12 12:26:13 -0400

    Added TODO for purpose of this branch

---
+++ src/test/java/com/thegingerbeardd/dndbot/utils/AbilityTypesTest.java
@@ -0,0 +1,17 @@
+package com.thegingerbeardd.dndbot.utils;
+
+import com.thegingerbeardd.dndbot.character.utils.fifthedition.AbilityTypes;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class AbilityTypesTest {
+
+    @Test
+    public void getTypeWorksForAllSixTypes() {
+        //TODO remove necessity for tests by removing the held value in the constructor and requisite getter
+        fail();
+    }
+
+}
