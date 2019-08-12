f3e3b382c8283135a1cce9959b53e53ae9f06567
Chris Mazur <christopher.mazur@fedex.com>
2019-08-12 12:57:38 -0400

    Created test class and added TODOs for completing this stage of the branch

---
+++ src/test/java/com/thegingerbeardd/dndbot/dice/DieTest.java
@@ -0,0 +1,11 @@
+package com.thegingerbeardd.dndbot.dice;
+
+public class DieTest {
+
+    //TODO fix making Random private in Dice
+    //TODO test RefreshRandomAlgorithm somehow (issue with it being private, now isn't it?)
+    //TODO test constructor functions
+    //TODO test roll method
+    //TODO test roll with modifier method
+
+}
