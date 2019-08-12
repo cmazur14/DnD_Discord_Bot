f971e0286c008d324edd40d9d2279444579d3396
Chris Mazur <christopher.mazur@fedex.com>
2019-08-12 12:40:37 -0400

    Implemented design change that resulted in necessary getType() method for AbilityTypes class. However, the string arguments have been removed, so essentially it is the same improvement, albeit with some necessary testing, which shall be implemented shortly

--- src/main/java/com/thegingerbeardd/dndbot/character/utils/fifthedition/AbilityTypes.java
+++ src/main/java/com/thegingerbeardd/dndbot/character/utils/fifthedition/AbilityTypes.java
@@ -1,20 +1,14 @@
 package com.thegingerbeardd.dndbot.character.utils.fifthedition;
 
 public enum AbilityTypes {
-    STR("strength"),
-    DEX("dexterity"),
-    CON("constitution"),
-    INT("intelligence"),
-    WIS("wisdom"),
-    CHA("charisma");
+    STR,
+    DEX,
+    CON,
+    INT,
+    WIS,
+    CHA;
 
-    private String val;
-
-    AbilityTypes(String val) {
-        this.val = val;
-    }
-
-    public String getType() {
-        return val;
+    public static AbilityTypes getAbility(String str) {
+        return AbilityTypes.valueOf(str.substring(0, 3).toUpperCase());
     }
 }
--- src/test/java/com/thegingerbeardd/dndbot/character/utils/fifthedition/AbilityTypesTest.java
+++ src/test/java/com/thegingerbeardd/dndbot/character/utils/fifthedition/AbilityTypesTest.java
@@ -4,6 +4,7 @@ import com.thegingerbeardd.dndbot.character.utils.fifthedition.AbilityTypes;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 
 public class AbilityTypesTest {
@@ -12,6 +13,35 @@ public class AbilityTypesTest {
     public void getTypeWorksForAllSixTypes() {
         //TODO remove necessity for tests by removing the held value in the constructor and requisite getter
         fail();
+    }
+
+    @Test
+    public void canParseEnumTypeFromAnyValidInput() {
+        assertEquals(AbilityTypes.STR, AbilityTypes.valueOf("str".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.STR, AbilityTypes.valueOf("STRENGTH".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.STR, AbilityTypes.valueOf("Strength".substring(0, 3).toUpperCase()));
+
+        assertEquals(AbilityTypes.DEX, AbilityTypes.valueOf("dex".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.DEX, AbilityTypes.valueOf("DEXTERITY".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.DEX, AbilityTypes.valueOf("Dexterity".substring(0, 3).toUpperCase()));
+
+        assertEquals(AbilityTypes.CON, AbilityTypes.valueOf("con".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.CON, AbilityTypes.valueOf("CONSTITUTION".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.CON, AbilityTypes.valueOf("Constitution".substring(0, 3).toUpperCase()));
+
+        assertEquals(AbilityTypes.INT, AbilityTypes.valueOf("int".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.INT, AbilityTypes.valueOf("INTELLIGENCE".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.INT, AbilityTypes.valueOf("Intelligence".substring(0, 3).toUpperCase()));
+
+        assertEquals(AbilityTypes.WIS, AbilityTypes.valueOf("wis".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.WIS, AbilityTypes.valueOf("WISDOM".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.WIS, AbilityTypes.valueOf("Wisdom".substring(0, 3).toUpperCase()));
+
+        assertEquals(AbilityTypes.CHA, AbilityTypes.valueOf("cha".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.CHA, AbilityTypes.valueOf("Charisma".substring(0, 3).toUpperCase()));
+        assertEquals(AbilityTypes.CHA, AbilityTypes.valueOf("CHARISMA".substring(0, 3).toUpperCase()));
+
+        assertNotEquals(AbilityTypes.STR, AbilityTypes.valueOf());
     }
 
 }
